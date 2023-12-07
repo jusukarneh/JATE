3c8b0e7843343c9714e2281e63b512a271bf4002
jusukarneh <jusudaddi@yahoo.com>
2023-12-06 22:53:22 -0500

    jate

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,27 +1,32 @@
 const butInstall = document.getElementById("buttonInstall");
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
-  });
+
+  // Store the triggered events
+  window.deferredPrompt = event;
+
+  // Remove the hidden class from the button.
+  butInstall.classList.toggle('hidden', false);
+});
 
 butInstall.addEventListener('click', async () => {
-  
-  const promptEvent = window.deferredPrompt;
 
-  if (!promptEvent) {
-   return;
-  }
+const promptEvent = window.deferredPrompt;
 
-  promptEvent.prompt();
-  
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
+if (!promptEvent) {
+ return;
+}
+
+// Show prompt
+promptEvent.prompt();
+
+// Reset the deferred prompt variable, it can only be used once.
+window.deferredPrompt = null;
+
+butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-  
-  window.deferredPrompt = null;
+// Clear prompt
+window.deferredPrompt = null;
 }); 
-
